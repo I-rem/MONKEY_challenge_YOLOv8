4fd53d69344a971267ce5cff67c3088aa0c3552b
İrem Ayşe Kayacıoğlu <72342267+I-rem@users.noreply.github.com>
2024-12-15 01:19:17 +0300

    Update main.py
    Start from root on main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import os
 if __name__ == "__main__":
     python_path = "/venv/bin/python"
 
-    subprocess.run([python_path, "./utils/make_pure_patches.py"])
-    subprocess.run([python_path, "./utils/results_with_normalization_roi.py"])
-    subprocess.run([python_path, "./utils/pixel_to_mm.py"])
+    subprocess.run([python_path, "/utils/make_pure_patches.py"])
+    subprocess.run([python_path, "/utils/results_with_normalization_roi.py"])
+    subprocess.run([python_path, "/utils/pixel_to_mm.py"])
