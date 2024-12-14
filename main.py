2cbd817ab8a7ff8d7e84a8db9501f9e6aae62b23
İrem Ayşe Kayacıoğlu <72342267+I-rem@users.noreply.github.com>
2024-12-14 10:42:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,3 @@ import os
 os.system("python ./utils/make_pure_patches.py")  
 os.system("python ./utils/results_with_normalization_roi.py")  
 os.system("python ./utils/pixel_to_mm.py")  
-
-os.remove("detected-inflammatory-cells.json")
-os.remove("detected-lymphocytes.json")
-os.remove("detected-monocytes.json")
